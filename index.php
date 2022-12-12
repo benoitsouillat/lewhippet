2cc5a0f05a7e08215c1bc12bff2db5d2c977e6ae
Benoit Souillat <tido.squat@gmail.com>
2022-12-12 10:14:06 +0100

    creating index.php -- and make structure of principal div or section

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Language" content="fr">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+    <meta name="description" content="Chiots Whippet à vendre - Découvrez les chiots whippet de la romance des damoiseaux">
+    <title>CHIOTS WHIPPET A VENDRE | Découvrez les chiots de la romance des damoiseaux</title>
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/script/style.css">
+    
+    <link rel="shortcut icon" type="image/ico" href="/script/dog.ico" />
+    
+    <script>
+        document.write('<link type="text/css"  rel="stylesheet" href="/script/style.css?dev=' + Math.floor(Math.random() * 10) + '" \><\//>');
+    </script>
+    <link rel="stylesheet" href="/css/main.css" type="text/css">
+    <!-- To force uncached data in style.css-->
+</head>
+<body>
+<header>
+    <div class="menu">
+        <span>lewhippet.com | </span>
+        <a class="menu_link" href="/index.php">Accueil</a>
+        |
+        <a class="menu_link" href="/sommaire.htm"> Sommaire</a>
+        |
+        <a class="menu_link" href="/contact/contact.html"> Contact </a>
+        |
+        <a class="menu_link" href="https://la-romance-des-damoiseaux.jimdosite.com/" target="_blank"> Mon Jimdo</a>
+        <br>
+        <br>
+        <br>
+        <!-- Passer le menu en require -->
+    </div>
+    <div class="infos_update">
+        <p>
+            Bienvenue à la Romance des Damoiseaux </p>
+    
+    
+    </div>
+</header>
+
+<main>
+        Salut je suis le main
+    <div> Bonjour je suis uen div <br>
+    <h1>Bonjour je suis un titre</h1></div>
+    
+    <div class="index__bb__dispo">
+        DIV CHIOTS DISPONIBLES
+    </div>
+    <div class="index__expo__result">
+        DIV RESULTATS EXPOS
+    </div>
+    <div class="fr3">
+        Lien FR3
+    </div>
+    <div class="accessories">DIV ACCESSOIRES
+    </div>
+    <div class="index__whippet">Div de nos whippets ?? Sliders de plusieurs repro ??</div>
+</main>
+<footer> <!-- Importer le footer via un require-->
+    <p>
+        Sabine Bourget, diplômée de l' Ecole Nationale du Chien N° ELEVEUR: 030488 / 51229 - N° MSA: 190 984 700 322
+        SIRET: 391602406 00020 <br>
+        Elevage de whippet déclaré aux services vétérinaires et à la préfecture de la Corrèze sous le n° 20050219
+    </p>
+
+</footer>
+<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
