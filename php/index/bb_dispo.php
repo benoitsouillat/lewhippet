cfa13eeab31d124459fe36faf80e05f888528513
Benoit Souillat <tido.squat@gmail.com>
2023-01-30 14:12:25 +0100

    update carousel index

--- php/index/bb_dispo.php
+++ php/index/bb_dispo.php
@@ -3,10 +3,10 @@
     <div id="index_bb" class="carousel slide" data-bs-ride="carousel" data-bs-pause="false" data-bs-interval="4000">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="/chiots_img/2022/Top_modèle/top_mod_carou2.jpg" alt="Bébés Whippets Disponibles">
+                <img src="/chiots_img//2023/Nefertiti/nefer_dispo2.jpg" alt="Joli Bébés Whippets LOF Disponibles">
             </div>
             <div class="carousel-item">
-                <img src="/chiots_img//2023/Nefertiti/nefer_dispo2.jpg" alt="Joli Bébés Whippets LOF Disponibles">
+                <img src="/chiots_img//2023/Oracle/325468647_876671950317355_3303173646111487727_n.jpg" alt="Joli Bébés Whippets LOF Disponibles">
             </div>
             <div class="carousel-item">
                 <img src="/chiots_img/2022/IMG_19891.jpg" alt="Joli Chiots Whippets Disponibles Joueur">
