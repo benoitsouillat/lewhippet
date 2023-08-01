d2b07eae617e88bf8aaa63b9be76ee432ddd7b7b
Benoit Souillat <tido.squat@gmail.com>
2023-08-01 11:48:45 +0200

    remove index.htm for php working

--- administration/login.php
+++ administration/login.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Se Connecter à la Romance des Damoiseaux</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
@@ -19,7 +18,7 @@
     <tr></tr>
     <form class="form_administration">
         <label for="username">Nom d'utilisateur</label>
-        <input class="form-control" name="username" id="username" />
+        <input type="text" class="form-control" name="username" id="username" />
 
     </form>
 </body>
