e4256691229144d44ed95d56515aee4da4a12fdf
Benoit Souillat <tido.squat@gmail.com>
2023-08-12 08:19:04 +0200

    updating index and bb_dispo - creating link Prodige

--- php/index/result_expo.php
+++ php/index/result_expo.php
@@ -1,44 +1,86 @@
-<section class="index__expo__result col-12 text-center">
-    <h2>Nos derniers résultats en expo</h2>
+<section class="index__expo__result col-12 text-center d-flex flex-column justify-content-start align-items-center">
+    <h2>Nos dernières actualités</h2>
 
-    <div>
+
+    <div id="prodige" class="w-75 text-center">
+        <h3>Prodige, un prénom bien choisi</h3>
+        <img src="/expo_img/prodige.jpg" alt="Prodige et ses chiots disponibles" /><br>
+
+        <p>Dans un tourbillon de succès et de bonheur, prodige, a brillamment remporté le Championnat de France 2023 de
+            la race Whippet. Cette incroyable compétition n’était que le début d’une aventure extraordinaire pour cette
+            championne hors pair.
+            Prodige est la fille de notre incroyable Impériale, alias The Pepette, une légende dans le monde des
+            Whippets. Elle a hérité du talent inné et de la grâce de sa mère, et elle a su se distinguer sur les rings
+            en devenant <b>Championne de France</b>.
+            Avec une grâce incomparable et une détermination sans faille, Prodige a été couronnée reine de la race en
+            France, attirant les éloges de tous les passionnés de chiens. Mais ce n’était pas la seule surprise que nous
+            réservait cette merveille.</p>
+        <p>
+            Peu de temps après son triomphe, notre reine a offert au monde une nouvelle raison de se réjouir en mettant
+            au monde six adorables bébés Whippets. Ces petites merveilles, fruit d’un amour inconditionnel entre elle et
+            le grand champion international de beauté, Monseigneur de la Romance des Damoiseaux, affectueusement
+            surnommé Poopy, ont immédiatement captivé les cœurs de tous ceux qui ont eu la chance de les rencontrer.
+        </p>
+        <p>
+            Nous sommes fiers de nos nouveaux trésors. ces chiots sont destinés à devenir des représentants
+            exceptionnels de leur race et porteront haut les couleurs de leurs ancêtres champions.
+
+            Avec des préparations déjà en cours pour de futures compétitions, les bébés Whippets grandiront dans un
+            environnement aimant et attentionné, bénéficiant des meilleurs soins pour développer tout leur potentiel.
+            Ils sont éduqué avec douceur et respect, leur permettant de s’épanouir dans leur propre personnalité unique.
+
+            Cette histoire de triomphe, d’amour et de naissance est le reflet même de la beauté du monde canin. La
+            chienne prodige, Reine de Beauté, et le grand champion international de beauté, Monseigneur de la Romance
+            des Damoiseaux, ont écrit ensemble un chapitre inoubliable dans l’histoire de la race Whippet.
+        </p>
+        <a href="/chiots/bb_dispo.php#prodige" class="btn btn-pink">Voir ses bébés</a>
+    </div>
+
+    <div class="w-75">
         <h3>Carcassonne CACIB 2023</h3>
         <p> Voici les résultats de la Romance des Damoiseaux à Carcassonne cette année</p>
-        <p> Zazie best in show champion les deux jours à Carcassonne + suprême best  in show
-            En bonne compagnie sur le podium avec la représentante de la mairie de Carcassonne et Mr Stibel ( juge venant tout droit de Macédoine )</p>
+        <p> Zazie best in show champion les deux jours à Carcassonne + suprême best in show
+            En bonne compagnie sur le podium avec la représentante de la mairie de Carcassonne et Mr Stibel ( juge
+            venant tout droit de Macédoine )</p>
         <img src="/expo_img/zazie_carcasonne_23.jpg" alt="Exposition Canine La Romance des Damoiseaux à Carcassonne">
     </div>
-    
-    <div>
+
+    <div class="w-75">
         <h3>Montluçon CACIB 2023</h3>
         <p> Une superbe exposition avec la Romance des Damoiseaux à Montluçon cette année 2023</p>
         <p> Voici nos résultats :
             <br> Impériale : 1ere excellent + CACIB V + BOB V + BOG V + BIS ( 3 ). Juge Mme melchior ( Luxembourg)
-            <br> Signature : 1ere excellent en classe jeune, CACIB Jeune + BOB + BOG + BIS Jugé par Mr Beccarelli et Mr Favre
+            <br> Signature : 1ere excellent en classe jeune, CACIB Jeune + BOB + BOG + BIS Jugé par Mr Beccarelli et Mr
+            Favre
             <br> Zero To One dite Zazie
-            Zazie 1ere excellent classe champion + Meilleur de race + Meilleur du groupe + BIS ( 3 ) jugé par Mr Beccarelli</p>
+            Zazie 1ere excellent classe champion + Meilleur de race + Meilleur du groupe + BIS ( 3 ) jugé par Mr
+            Beccarelli
+        </p>
         <p>Jugés par Mr Barjot parmi 34 Whippets incrits</p>
         <br>
         <p>Journée géniale en compagnie du soutien de La Romance des Damoiseaux, merci encore à tous.</p>
         <img src="/expo_img/zazie_bis_montlucon23.jpg" alt="Exposition Canine La Romance des Damoiseaux à Montlucon">
     </div>
-    
-    <div>
+
+    <div class="w-75">
         <h3>Perpignan CACIB 2023</h3>
         <p> Un excellent week-end avec la Romance des Damoiseaux à Perpignan cette année 2023</p>
         <p> Voici nos résultats :
             <br> Impériale : Meilleur Lévrier de l'exposition en vétérans <br> Un grand combat avec ma pepette !
             <br> Signature : Meilleur du groupe Lévrier en classe Jeune
-            <br> Zero To One dite Zazie s'est classée meilleur champion et meilleur de race ainsi que deuxième meilleur du groupe des lévriers
-            <br> Yamamoto dit Yam :  Meilleur Baby de l'exposition et Best-in Show le Dimanche</p>
+            <br> Zero To One dite Zazie s'est classée meilleur champion et meilleur de race ainsi que deuxième meilleur
+            du groupe des lévriers
+            <br> Yamamoto dit Yam : Meilleur Baby de l'exposition et Best-in Show le Dimanche
+        </p>
         <p>Jugés par Mr Barjot parmi 34 Whippets incrits</p>
         <br>
-        <p>Encore une journée trés intense en émotion, et je remercie tout le monde qui m'a soutenue de près ou de loin.</p>
+        <p>Encore une journée trés intense en émotion, et je remercie tout le monde qui m'a soutenue de près ou de loin.
+        </p>
         <img src="/expo_img/Yam_Perpi_2023.jpg" alt="Exposition Canine La Romance des Damoiseaux">
     </div>
 
-    
-   <!-- <div>
+
+    <!-- <div class="w-75">
         <h3>Nantes 2022</h3>
         <p>Une exposition pleine de succès à l'exposition intrnationale de Nantes 2022</p>
         <p>Voici nos résultats :
