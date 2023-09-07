ec0906d996de620c2edcc4203f789747507d3962
benoitsouillat <tido.squat@gmail.com>
2023-09-07 09:14:41 +0200

    update gitignore to simplify export project and update bddscript

---
+++ secret/bddScript.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once(__DIR__ . "/connexion.php"); // PDO FILE returning an instance name $conn
+
+$table_users = "CREATE TABLE `damoiseaux_php`.`users` (`id` INT NOT NULL AUTO_INCREMENT, `email` VARCHAR(50) NOT NULL, `password` VARCHAR(255) NOT NULL, `img_profile_path` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+
+$table_puppies = "CREATE TABLE `damoiseaux_php`.`puppies` (`id` INT NOT NULL AUTO_INCREMENT, `name` VARCHAR(50) NOT NULL, `sex` VARCHAR(10) NOT NULL, `available` VARCHAR(20) NOT NULL, `description` VARCHAR(255), `main_img_path` VARCHAR(255) NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+
+
+//$conn->exec($table_users);
+//$conn->exec($table_puppies);
+
+/* Insertion des données de test */
+
+function random_status()
+{
+    $rand = rand(1, 2);
+    if ($rand === 1) {
+        return 'Disponible';
+    } else {
+        return 'Réservé';
+    }
+}
+function random_sex()
+{
+    $rand = rand(1, 2);
+    if ($rand === 1) {
+        return 'femelle';
+    } else {
+        return 'male';
+    }
+}
+
+
+$path_img = "../puppies_img/photo.jpg";
+$rand_status = random_status();
+$rand_sex = random_sex();
+
+$test_puppies = "INSERT INTO `puppies` (name, sex, available, description, main_img_path) VALUES ('Geroges', '$rand_sex', '$rand_status', 'Ceci est une description !!' , '$path_img')";
+$conn->exec($test_puppies);
