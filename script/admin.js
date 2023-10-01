c4e753abfed8e5e38a98f4a0869252a7cfb8356c
benoitsouillat <tido.squat@gmail.com>
2023-10-01 10:41:45 +0200

    add getMethod on form to prepare Update Entity - Add JS to prepare Input when no is checked on breeder

--- script/admin.js
+++ script/admin.js
@@ -11,10 +11,24 @@ const confirmDeleteRepro = (id, name) => {
 }
 let reproDiv = document.getElementById('repro_breeder');
 let breederReproNoButton = document.getElementById('breeder_no');
+let breederReproYesButton = document.getElementById('breeder_yes');
 let inputReproName = document.createElement('input');
 inputReproName.placeholder = "Entrez l'affixe du chien";
 inputReproName.className = 'form-control';
 inputReproName.name = 'repro_breeder';
-breederReproNoButton.addEventListener('click', () => {
-    reproDiv.appendChild(inputReproName);
-});
+
+if (breederReproNoButton) {
+    if (breederReproNoButton.checked) {
+        inputReproName.value = breederReproNoButton.value;
+        reproDiv.appendChild(inputReproName);
+    }
+    breederReproNoButton.addEventListener('click', (e) => {
+        reproDiv.appendChild(inputReproName);
+    });
+    breederReproYesButton.addEventListener('click', (e) => {
+        console.info(e.target.value);
+        if (inputReproName) {
+            reproDiv.removeChild(inputReproName);
+        }
+    })
+}
