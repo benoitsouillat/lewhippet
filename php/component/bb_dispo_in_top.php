63d9b40a6fa8ddf95c20e13e88db223b12ab90b7
Benoit Souillat <dev.souillat@gmail.com>
2024-01-05 19:09:18 +0100

    some css on bb_dispo.php

--- php/component/bb_dispo_in_top.php
+++ php/component/bb_dispo_in_top.php
@@ -1,62 +1,113 @@
 <aside class="puppy-solo">
-
-    <div class="card">
+    <div class="card"> <!-- Un Rêve -->
         <figure>
-            <div class="diapo-container justify-content-center" data-speed="3500" data-dog-id="jean">
-                <div class="diapo diapo-jean">
-                    <img src="../../puppies_img/9-malen°2.jpg" alt="Chiot Whippet Disponible">
-                    <img src="../../puppies_img/9-malen°2.jpg" alt="Chiot Whippet Disponible">
+            <div class="diapo-container justify-content-center" data-speed="3500" data-dog-id="unreve">
+                <div class="diapo diapo-unreve">
+                    <img src="../../puppies_img/puppy-solo/unreve.jpg" alt="Chiot Whippet Disponible">
+                    <img src="../../puppies_img/puppy-solo/unreve2.jpg" alt="seconde image d'un chiot whippet" loading="lazy">
+                    <img src="../../puppies_img/puppy-solo/unreve3.webp" alt="Chiot Whippet Disponible" loading="lazy">
+                    <img src="../../puppies_img/puppy-solo/unreve4.jpg" alt="Chiot Whippet Disponible" loading="lazy">
+                    <img src="../../puppies_img/puppy-solo/unreve.jpg" alt="Chiot Whippet Disponible">
                 </div>
             </div>
             <div class="arrow-div">
                 <button class="left-arrow">
-                    <span class="bi bi-caret-left bi-caret-left-jean"></span>
+                    <span class="bi bi-caret-left bi-caret-left-unreve"></span>
                 </button>
                 <button class="right-arrow">
-                    <span class="bi bi-caret-right bi-caret-right-jean"></span>
-                </button>
-            </div>
-            <figcaption>
-                <div class="d-flex flex-row justify-content-around align-items-center pr-4 pl-4 mt-3 mb-3 labels">
-                    <h4><span class="text-center">Jean </span> <i class="bi bi-gender-male"> </i> </h4>
-                    <p class="alert alert-success">Disponible</p>
-                </div>
-                <p class="description"></p>
-            </figcaption>
-        </figure>
-    </div>
-
-    <div class="card">
-        <figure>
-            <div class="diapo-container justify-content-center" data-speed="3500" data-dog-id="harry">
-                <div class="diapo diapo-harry">
-                    <img src="../../puppies_img/harry-male3.jpg" alt="Chiot Whippet Disponible" data-pagespeed-url-hash="780851674">
-                    <img src="../../puppies_img/harry-hpYF.jpg" alt="seconde image d'un chiot whippet" loading="lazy" data-pagespeed-url-hash="147814807">
-                    <img src="../../puppies_img/harry-male3.jpg" alt="Chiot Whippet Disponible" loading="lazy" data-pagespeed-url-hash="780851674" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
-                </div>
-            </div>
-            <div class="arrow-div">
-                <button class="left-arrow">
-                    <span class="bi bi-caret-left bi-caret-left-harry"></span>
-                </button>
-                <button class="right-arrow">
-                    <span class="bi bi-caret-right bi-caret-right-harry"></span>
+                    <span class="bi bi-caret-right bi-caret-right-unreve"></span>
                 </button>
             </div>
             <figcaption>
                 <div class="header-caption-container labels">
-                    <h4><span class="text-center">Harry</span> <i class="bi bi-gender-male"> </i> </h4>
-                    <p class="alert alert-success"> Disponible</p>
+                    <h4><span class="text-center">Un rêve</span> <i class="bi bi-gender-male"> </i> </h4>
+                    <p class="alert alert-success">Disponible</p>
                 </div>
                 <div class="mother-description-container">
-                    <p class="mother-description"> Issu de Mam’zelle ( <span class="badge badge-pink">ADN Vérifiée</span> <span class="badge badge-blue">Championne</span> ) </p>
+                    <p class="mother-description">Issu de Promesse (
+                        <span class="badge badge-pink">ADN Vérifiée</span>
+                        <!-- <span class="badge badge-blue">Championne</span> -->
+                        )
+                    </p>
                 </div>
                 <div class="puppy-description-container">
-                    <p class="puppy-description">Male 3 bicolore beige né le 23 septembre 2023
-                        Superbe male avec gros potentiel exposition de beauté
-                        Très calme et posé. <br>
-                        Doté d'un caractère très tendre et joueur
-                        Il aime la compagnie et sait resté tranquille et seul</p>
+                    <p class="puppy-description">Adorable bébé whippet bringé sur un fond chocolat. rare coloris. Très joueur et très affectueux. Sage et coquin à ses heures . Chiot avec gros potentiel pour exposition de beauté</p>
+                    <p class="puppy-price">Prix 1500 euros</p>
+                </div>
+            </figcaption>
+        </figure>
+    </div>
+    <div class="card"> <!-- Mâle 3 - MamZelle -->
+        <figure>
+            <div class="diapo-container justify-content-center" data-speed="3500" data-dog-id="mammal3">
+                <div class="diapo diapo-mammal3">
+                    <img src="../../puppies_img/puppy-solo/mammal3.jpg" alt="Chiot Whippet Disponible">
+                    <img src="../../puppies_img/puppy-solo/mammal32.jpg" alt="seconde image d'un chiot whippet" loading="lazy">
+                    <img src="../../puppies_img/puppy-solo/mammal3.jpg" alt="Chiot Whippet Disponible">
+                </div>
+            </div>
+            <div class="arrow-div">
+                <button class="left-arrow">
+                    <span class="bi bi-caret-left bi-caret-left-mammal3"></span>
+                </button>
+                <button class="right-arrow">
+                    <span class="bi bi-caret-right bi-caret-right-mammal3"></span>
+                </button>
+            </div>
+            <figcaption>
+                <div class="header-caption-container labels">
+                    <h4><span class="text-center">Mâle N°3</span> <i class="bi bi-gender-male"> </i> </h4>
+                    <p class="alert alert-success">Disponible</p>
+                </div>
+                <div class="mother-description-container">
+                    <p class="mother-description">Issu de Mam'Zelle (
+                        <span class="badge badge-pink">ADN Vérifiée</span>
+                        <span class="badge badge-blue">Championne</span>
+                        )
+                    </p>
+                </div>
+                <div class="puppy-description-container">
+                    <p class="puppy-description">Male 3 bicolore beige né le 23 septembre 2023 <br>
+                        Superbe male avec gros potentiel exposition de beauté Très calme et posé
+                        <br> Doté d'un caractère très tendre et joueur, il aime la compagnie et sait resté tranquille et seul
+                    </p>
+                    <p class="puppy-price">Prix 1500 euros</p>
+                </div>
+            </figcaption>
+        </figure>
+    </div>
+    <div class="card"> <!-- Un Charme -->
+        <figure>
+            <div class="diapo-container justify-content-center" data-speed="3500" data-dog-id="uncharme">
+                <div class="diapo diapo-uncharme">
+                    <img src="../../puppies_img/puppy-solo/uncharme.jpg" alt="Chiot Whippet Disponible">
+                    <img src="../../puppies_img/puppy-solo/uncharme.jpg" alt="Chiot Whippet Disponible">
+                </div>
+            </div>
+            <div class="arrow-div">
+                <button class="left-arrow">
+                    <span class="bi bi-caret-left bi-caret-left-uncharme"></span>
+                </button>
+                <button class="right-arrow">
+                    <span class="bi bi-caret-right bi-caret-right-uncharme"></span>
+                </button>
+            </div>
+            <figcaption>
+                <div class="header-caption-container labels">
+                    <h4><span class="text-center">Un Charme</span> <i class="bi bi-gender-male"> </i> </h4>
+                    <p class="alert alert-success">Disponible</p>
+                </div>
+                <div class="mother-description-container">
+                    <p class="mother-description">Issu de Promesse (
+                        <span class="badge badge-pink">ADN Vérifiée</span>
+                        <!-- <span class="badge badge-blue">Championne</span> -->
+                        )
+                    </p>
+                </div>
+                <div class="puppy-description-container">
+                    <p class="puppy-description">Adorable chiot de 4 mois et demi et très attachant.
+                        Il adore la compagnie mais sait rester seul. Il adore aussi partager les balades et les jeux. <br> Il est habitué aux chats et aux autres chiens. Il est câlin et affectueux mais sait aussi rester sage et discret
+                    </p>
                     <p class="puppy-price">Prix 1500 euros</p>
                 </div>
             </figcaption>
