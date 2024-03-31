1b62e3fd384844e6467c51a80ca18a05fd769d43
Benoit Souillat <dev.souillat@gmail.com>
2024-03-31 22:05:49 +0200

    update 31 march remove charme

--- php/component/bb_dispo_in_top.php
+++ php/component/bb_dispo_in_top.php
@@ -5,8 +5,7 @@
             <div class="diapo-container justify-content-center" data-speed="3500" data-dog-id="unreve">
                 <div class="diapo diapo-unreve">
                     <img src="../../puppies_img/puppy-solo/unreve.jpg" alt="Chiot Whippet Disponible">
-                    <img src="../../puppies_img/puppy-solo/unreve2.jpg" alt="seconde image d'un chiot whippet"
-                        loading="lazy">
+                    <img src="../../puppies_img/puppy-solo/unreve2.jpg" alt="seconde image d'un chiot whippet" loading="lazy">
                     <img src="../../puppies_img/puppy-solo/unreve3.jpg" alt="Chiot Whippet Disponible" loading="lazy">
                     <img src="../../puppies_img/puppy-solo/unreve4.jpg" alt="Chiot Whippet Disponible" loading="lazy">
                     <img src="../../puppies_img/puppy-solo/unreve.jpg" alt="Chiot Whippet Disponible">
@@ -36,48 +35,6 @@
                     <p class="puppy-description">Pour sa première exposition internationale, Un rêve remporte le titre
                         de meilleur baby les deux jours à Perpignan samedi 20 et dimanche 21 janvier 2024. <br> Bravo
                         petit bébé qui s'est montré exemplaire pendant ce grand voyage et ce défilé de beauté </p>
-                    <p class="puppy-price">Prix 1500 euros</p>
-                </div>
-            </figcaption>
-        </figure>
-    </div>
-    <div class="card">
-        <!-- Un Charme -->
-        <figure>
-            <div class="diapo-container justify-content-center" data-speed="3500" data-dog-id="uncharme">
-                <div class="diapo diapo-uncharme">
-                    <img src="../../puppies_img/puppy-solo/uncharme.jpg" alt="Chiot Whippet Disponible">
-                    <img src="../../puppies_img/puppy-solo/uncharme2.jpg" alt="Chiot Whippet Disponible">
-                    <img src="../../puppies_img/puppy-solo/uncharme3.jpg" alt="Chiot Whippet Disponible">
-                    <img src="../../puppies_img/puppy-solo/uncharme.jpg" alt="Chiot Whippet Disponible">
-                </div>
-            </div>
-            <div class="arrow-div">
-                <button class="left-arrow">
-                    <span class="bi bi-caret-left bi-caret-left-uncharme"></span>
-                </button>
-                <button class="right-arrow">
-                    <span class="bi bi-caret-right bi-caret-right-uncharme"></span>
-                </button>
-            </div>
-            <figcaption>
-                <div class="header-caption-container labels">
-                    <h4><span class="text-center">Un Charme</span> <i class="bi bi-gender-male"> </i> </h4>
-                    <p class="alert alert-success">Disponible</p>
-                </div>
-                <div class="mother-description-container">
-                    <p class="mother-description">Issu de Promesse (
-                        <span class="badge badge-pink">ADN Vérifiée</span>
-                        <!-- <span class="badge badge-blue">Championne</span> -->
-                        )
-                    </p>
-                </div>
-                <div class="puppy-description-container">
-                    <p class="puppy-description">Adorable chiot né le 05 Août 2023 et très attachant.
-                        Il adore la compagnie mais sait rester seul. Il adore aussi partager les balades et les jeux.
-                        <br> Il est habitué aux chats et aux autres chiens. Il est câlin et affectueux mais sait aussi
-                        rester sage et discret
-                    </p>
                     <p class="puppy-price">Prix 1500 euros</p>
                 </div>
             </figcaption>
